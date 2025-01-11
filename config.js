8c8030bcb86512bdb9a91fd1e3273ea45238d899
kkr4ssh <e70345118@gmail.com>
2025-01-11 14:22:13 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ import axios from 'axios';
 import moment from 'moment-timezone';
 
 global.owner = [
-  ['393756447945', '𝐄ИΞM𝕀Ξ𝐒 𝐁Ꮻ𝐓', true],
+  ['15128899641', '𝐄ИΞM𝕀Ξ𝐒 𝐁Ꮻ𝐓', true],
   ['377607935810'],
   ['393756447945'],
   ['393756447945']
